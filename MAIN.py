cdcce03ce42f4c5fa2577870ce297adc95b54d19
ogheat <ogheat1337@gmail.com>
2021-09-30 20:32:33 +0200

    fixed MAIN.py

--- MAIN.py
+++ MAIN.py
@@ -3,19 +3,18 @@ from gtts import gTTS
 
 print("It is PDF to MP3 converter")
 name = input("Print name of your pdf-file(without .pdf) ")
-lang = input("Print language of the book in the IETF format(ex. 'ru' for russian, 'en' for english")
+lang = input("Print language of the book in the IETF format(ex. 'ru' for russian, 'en' for english ")
 print("Loading...If the file is large, it may take some time (up to 15 minutes)")
 all_text = ""
 pdf = pdfplumber.open(f'{name}.pdf')
-for i in range(0,(len(pdf.pages) - 1)):
+for i in range(0,(len(pdf.pages))):
     page = pdf.pages[i]
     text = page.extract_text()
     if type(text) == str:
         all_text += text
 pdf.close()
-
 tts = gTTS(all_text,lang=f"{lang}")
-tts.save('results/result.mp3')
+tts.save('result/result.mp3')
 print("File result.mp3 successful saved to the result folder ")
 
 
