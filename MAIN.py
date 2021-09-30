91f7609e36b3f2795543a052c6709c782f054a12
ogheat <ogheat1337@gmail.com>
2021-09-30 19:31:33 +0200

    change module from PyPDF2 to pdfplumber to support another languages

--- MAIN.py
+++ MAIN.py
@@ -1,24 +1,21 @@
-import PyPDF2
+import pdfplumber
 from gtts import gTTS
-
-
 
 print("It is PDF to MP3 converter")
 name = input("Print name of your pdf-file(without .pdf) ")
-
-
+lang = input("Print language of the book in the IETF format(ex. 'ru' for russian, 'en' for english")
+print("Loading...If the file is large, it may take some time (up to 15 minutes)")
 all_text = ""
-pdffileobj = open(f'{name}.pdf', 'rb')
-pdfreader = PyPDF2.PdfFileReader(pdffileobj)
-x = pdfreader.numPages
-for i in range(x):
+pdf = pdfplumber.open(f'{name}.pdf')
+for i in range(0,(len(pdf.pages) - 1)):
+    page = pdf.pages[i]
+    text = page.extract_text()
+    if type(text) == str:
+        all_text += text
+print(all_text)
+pdf.close()
 
-    pageobj = pdfreader.getPage(i)
-    text = pageobj.extractText()
-    all_text += text
-
-
-tts = gTTS(all_text)
+tts = gTTS(all_text,lang=f"{lang}")
 tts.save('results/result.mp3')
 print("File result.mp3 successful saved to the results folder ")
 
